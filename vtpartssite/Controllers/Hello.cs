bbc6e9472d1a938d9c1cc772b7eccee94fa18275
ukl2808 <107134301+ukl2808@users.noreply.github.com>
2024-11-09 11:13:36 +0200

    added controller hello world

---
+++ vtpartssite/Controllers/Hello.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vtpartssite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
